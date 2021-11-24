07e4da97534fd9aa297ac610c52d5092d9913c65
redgoose <scripter@me.com>
2021-11-24 23:51:12 +0900

    db sql 약간 수정

--- resource/db.default.sql
+++ resource/db.default.sql
@@ -9,8 +9,8 @@ create table `slideshow` (
   `slideshow` mediumtext not null,
   `password` varchar(100) not null comment 'Slideshow password',
   `thumbnail` varchar(100) default null comment 'Thumbnail image address',
-  `visible` tinyint(1) default 1 comment 'visible item',
-  `hit` int(11) default 0 comment 'visit count',
+  `visible` tinyint(1) default 1 not null comment 'visible item',
+  `hit` int(11) default 0 not null comment 'visit count',
   `regdate` datetime not null,
   `update` datetime not null
 ) engine=InnoDB default charset=utf8mb4;
